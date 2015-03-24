30a9b78e7a41a6f864a79f1909192d5f38c1fce0
Spencer Lyon <spencerlyon2@gmail.com>
2015-03-24 11:04:26 -0400

    TEST: added test example from Greene 2012 (compared to stata results)

---
+++ test/gamma_dist.jl
@@ -0,0 +1,18 @@
+######################################################
+### This test implements example 13.5 in Greene (2012)
+######################################################
+
+# this is the data from Example C.1 in Greene 2012
+income_data = [20.5, 31.5, 47.7, 26.2, 44, 8.28, 30.8, 17.2, 19.9, 9.96,
+               55.8, 25.2, 29, 85.5, 15.1, 28.5, 21.4, 17.7, 6.42, 84.9]
+
+function g_gamma(θ)
+    P, λ = θ[1], θ[2]
+    hcat(income_data - P / λ,
+         income_data.^2 - P*(P+1) / (λ^2),
+         log(income_data) + log(λ) - digamma(P),
+         1./income_data - λ/(P - 1))
+end
+
+two_step = GMM.gmm(g_gamma, [2.40, 0.08], [0.0, 0.0], [Inf, Inf], eye(4);
+                   mgr=GMM.TwoStepGMM())
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,31 @@ using FactCheck
 
 facts("Testing basic interface") do
     context("Test from vingette for the R gmm package.") do
-        include("normal_dist.jl")        
+        include("normal_dist.jl")
         cft = [3.84376, 2.06728]
         cfe = coef(step_1)
         sdt = [0.06527666675890574,0.04672629429325811]
         sde = stderr(step_1)
         for j = 1:2
             @fact cfe[j] => roughly(cft[j])
-            @fact sde[j] => roughly(sdt[j])            
+            @fact sde[j] => roughly(sdt[j])
         end
-        @fact GMM.J_test(step_2_iid) => (1.4398836656920428,0.23015816678811782)
+        Je, pe = GMM.J_test(step_2_iid)
+        Jt, pt = (1.4398836656920428,0.23015816678811782)
+        @fact Je => roughly(Jt)
+        @fact pe => roughly(pt)
+    end
+
+    context("Example 13.5 from Greene (2012) -- verified with Stata") do
+        include("gamma_dist.jl")
+        cf_stata = [3.358432, .1244622]
+        cfe = coef(two_step)
+        for j = 1:2
+            @fact cfe[j] => roughly(cf_stata[j], atol=1e-3)
+        end
+        Je, pe = GMM.J_test(two_step)
+        Jt, pt = (1.97522, 0.3725)
+        @fact Je => roughly(Jt, atol=1e-3)
+        @fact pe => roughly(pt, atol=1e-3)
     end
 end
