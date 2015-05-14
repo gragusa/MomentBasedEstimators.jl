687e1db9ae391a3650f5a6f9be49a6b773c12b36
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2015-05-14 17:09:05 +0200

    Change coherent with the new version of the J_test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,13 @@ facts("Testing basic interface") do
             @fact sde[j] => roughly(sdt[j])
         end
         Je, pe = GMM.J_test(step_iid_mgr)
-        Jt, pt = (1.4398836656920428,0.23015816678811782)
+        ## This is the objective value, which is the J-test
+        ## for other softwares
+        ov     = 1.4398836656920428
+        Jt, pt = (1.4378658264483137,0.23048500853597673)
         @fact Je => roughly(Jt, atol = 0.01)
         @fact pe => roughly(pt, atol = 0.01)
+        @fact objval(step_iid_mgr) => roughly(ov, atol = 0.1)
     end
 
     context("Example 13.5 from Greene (2012) -- verified with Stata") do
@@ -26,8 +30,13 @@ facts("Testing basic interface") do
             @fact cfe[j] => roughly(cf_stata[j], atol=1e-3)
         end
         Je, pe = GMM.J_test(two_step)
-        Jt, pt = (1.97522, 0.3725)
+        Jt, pt = (1.7433378725860427,0.41825292894063326)
+        ## This is the stata J-test
+        ov = 1.97522
         @fact Je => roughly(Jt, atol=1e-3)
         @fact pe => roughly(pt, atol=1e-3)
+        @fact objval(two_step) => roughly(ov, atol = 0.1)
     end
 end
+
+FactCheck.exitstatus()
